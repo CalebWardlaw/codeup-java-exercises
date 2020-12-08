e1acbca3349cc1ce24c5a27c8f7c605d814bcacd
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-12-08 12:13:42 -0600

    created dish class

---
+++ src/dish.java
@@ -0,0 +1,15 @@
+
+//why can't i put public?
+class dish {
+   public static Integer costInCents;
+   public static String nameOfDish;
+   public static Boolean wouldRecommend;
+
+
+    public static void printSummary(String[]args){
+        System.out.printf("Cost: %n\n Name: %s\n Recommend: %b\n" costInCents, nameOfDish, wouldRecommend);
+    }
+
+}
+
+
