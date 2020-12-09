650ad015256675d4c70ccbc8afa680163f092d04
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-12-09 10:08:58 -0600

    created getName setName and sayHello methods

--- src/dish.java
+++ src/dish.java
@@ -1,15 +1,27 @@
 
 //why can't i put public?
-class dish {
-   public static Integer costInCents;
-   public static String nameOfDish;
-   public static Boolean wouldRecommend;
+ class Dish {
+   public  Integer costInCents;
+   public  String nameOfDish;
+   public  Boolean wouldRecommend;
 
 
-    public static void printSummary(String[]args){
-        System.out.printf("Cost: %n\n Name: %s\n Recommend: %b\n" costInCents, nameOfDish, wouldRecommend);
+    public void printSummary(String[] args){
+        System.out.printf("Cost: %d\n Name: %s\n Recommend: %b\n" ,costInCents, nameOfDish, wouldRecommend);
     }
 
+//    public static void main(String[]args){
+//         class DishTest {
+//             Dish soup = new Dish();
+//             soup.costInCents = 100;
+//        }
+//
+//
+//     }
+
 }
+
+
+
 
 
