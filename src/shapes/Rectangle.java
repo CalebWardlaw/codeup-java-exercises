8dd53eb0a9c11bfae13220f380fac0460e83826d
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-12-14 19:45:47 -0600

    created area and perimeter methods

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,30 @@
+package shapes;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Rectangle {
+    //Initiate
+    protected int length;
+    protected int width;
+    //Construct
+    public Rectangle(int length, int width){
+    this.length = length;
+    this.width = width;
+    }
+    //Perimeter
+
+    public int getPerimeter(){
+        int perimeter =  (2 * length) + (2 * width);
+        System.out.println("Calculating perimeter\n");
+        return perimeter;
+    }
+
+    public int getArea(){
+        int area = length*width;
+        System.out.println("Calculating area\n");
+        return area;
+    }
+    //Inside of your shapes directory, create a class named Rectangle. It should have protected
+    // properties for both length and width. Rectangle should define a constructor that
+    // accepts two numbers for length and width, and sets those properties.
+}
