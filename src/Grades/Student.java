570a8fc46f1c547209412343743c4abed2d63c47
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-12-17 10:52:45 -0600

    started student class

---
+++ src/Grades/Student.java
@@ -0,0 +1,16 @@
+package Grades;
+import java.util.Arrays;
+
+public class Student {
+    private String name;
+    private int[] grade;
+
+    grade = new int[];
+
+
+
+
+    public static void main(String[]args){
+
+    }
+}
