e1e10d611e1fef8c59c85693dd06baf0d2f49508
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-12-29 14:48:17 -0600

    created getters and setters for length/width

--- src/shapes/Quadrilateral.java
+++ src/shapes/Quadrilateral.java
@@ -1,4 +1,39 @@
 package shapes;
 
 public abstract class Quadrilateral extends Shape implements Measurable {
+    //Props
+    protected double length;
+    protected double width;
+
+    //Constructor
+    public Quadrilateral(double length, double width) {
+        //set properties
+        this.length = length;
+        this.width = width;
+    }
+
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    //Getters
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    //Setters
+    public abstract void setLength(double length);
+
+    public abstract void setWidth(double width);
 }
