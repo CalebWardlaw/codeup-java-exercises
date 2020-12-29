a8a794e1912038fb4f4495d3554fcb6e7bcee56d
Caleb Wardlaw <calebwardlaw@gmail.com>
2020-12-29 14:36:13 -0600

    created Shape class

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,19 +1,19 @@
 package shapes;
 
 public class Square extends Rectangle{
-    //Must call parent constructor using super, implemented by IDE
-    public Square(int side) {
-        super(side, side);
-    }
-
-    public int getArea(){
-        System.out.println("Calculating square area:");
-        return length * length;
-    }
-
-    public int getPerimeter(){
-        System.out.println("Calculating square perimeter");
-        return 4*length;
-    }
+//    //Must call parent constructor using super, implemented by IDE
+//    public Square(int side) {
+//        super(side, side);
+//    }
+//
+//    public int getArea(){
+//        System.out.println("Calculating square area:");
+//        return length * length;
+//    }
+//
+//    public int getPerimeter(){
+//        System.out.println("Calculating square perimeter");
+//        return 4*length;
+//    }
 
 }
